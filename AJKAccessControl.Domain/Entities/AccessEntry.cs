8b6c1387d7c5088130c15da7829078b0bcfcc42c
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2024-10-13 23:52:25 +0200

    Empty razor pages

--- AJKAccessControl.Domain/Entities/AccessEntry.cs
+++ AJKAccessControl.Domain/Entities/AccessEntry.cs
@@ -4,8 +4,8 @@ namespace AJKAccessControl.Domain.Entities
     public class AccessEntry
     {
         public int Id { get; set; }
-        public int EntitityId { get; set; }
-        public EntitityType EntitityType { get; set; }
+        public int EntityId { get; set; }
+        public EntitityType EntityType { get; set; }
         public DateTime EntryTime { get; set; } = DateTime.UtcNow;
         public DateTime? ExitTime { get; set; }
         public string Description { get; set; } = string.Empty;
