dfd9884c7ee9c5aadc0f0cec3437e4f66a8ce886
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2024-10-22 21:23:13 +0200

    Change password form

--- AJKAccessControl.Shared/DTOs/ChangePasswordDto.cs
+++ AJKAccessControl.Shared/DTOs/ChangePasswordDto.cs
@@ -6,7 +6,11 @@ namespace AJKAccessControl.Shared.DTOs
     {
         [Required]
         public string UserName { get; set; } = string.Empty;
-        [Required]
+
+        [Required(ErrorMessage = "Hasło jest wymagane.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Hasło musi mieć co najmniej 6 znaków.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).+$",
+        ErrorMessage = "Hasło musi zawierać co najmniej jedną wielką literę, jedną małą literę, jedną cyfrę oraz jeden znak niealfanumeryczny.")]
         public string Password { get; set; } = string.Empty;
     }
 }
